44156fbac540753e4717c50057e779188737b006
yukimi.y <yyy.yukimi@gmail.com>
2021-05-17 08:57:34 +0900

    初期データ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+post_params = [
+  {
+    title: "React",
+    content: "ユーザインターフェース構築のための JavaScript ライブラリ"
+  },
+  {
+    title: "Vue.js",
+    content: "The Progressive JavaScript Framework"
+  },
+  {
+    title: "Angular",
+    content: "モバイルとデスクトップ、ひとつのフレームワーク"
+  }
+]
+
+Post.delete_all
+Post.create!(post_params)
+puts "初期データの投入に成功しました!"
