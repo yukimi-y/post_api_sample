b17553ce5ec721c1a649caceaa6746bd3c42421d
yukimi.y <yyy.yukimi@gmail.com>
2021-05-18 08:26:42 +0900

    投稿機能実装

--- html/post_js_sample/script.js
+++ html/post_js_sample/script.js
@@ -42,5 +42,58 @@ const displayPosts = async () => {
   }
 };
 
+// 投稿ボタンの DOM 要素オブジェクトを取得
+const postButton = document.getElementById('post-button');
+// タイトル入力フィールドの DOM 要素オブジェクトを取得
+const titleElement = document.getElementById('post-title');
+// 内容入力エリアの DOM 要素オブジェクトを取得
+const contentElement = document.getElementById('post-content');
+
+// 入力内容をAPI側のサーバーに送信して保存する関数
+const registerPost = async () => {
+  // ローカルの Rails サーバーの posts#create に対応するURL
+  const url = `${baseURL}/posts`;
+  // 入力内容をAPI側が受け取れるパラメータ形式に加工
+  const postParams = {
+    post: {
+      title: titleElement.value,
+      content: contentElement.value,
+    },
+  };
+
+  // API側のサーバーに送信
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(postParams),
+  });
+
+  if (!response.ok) {
+    throw new Error(`${response.status} (${response.statusText})`);
+  }
+
+  const post = await response.json();
+  return post;
+};
+
+// フォームを送信し，入力内容を消去する関数
+const postForm = async () => {
+  try {
+    // 入力内容をAPI側のサーバーに送信
+    const post = await registerPost();
+    // 入力内容を投稿リストに追加
+    addPost(post);
+    // 入力内容を消去
+    titleElement.value = contentElement.value = '';
+  } catch (e) {
+    alert(e);
+  }
+};
+
+// ボタンをクリックしたときにフォームを送信
+postButton.addEventListener('click', postForm);
+
 // 起動時に投稿一覧表示
 displayPosts();
